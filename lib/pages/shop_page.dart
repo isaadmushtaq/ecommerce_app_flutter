d5af74fef32186bd90ec16348d9e99769dc04255
Saad Mushtaq <saadmushtaq502@gmail.com>
2024-03-06 23:45:04 +0500

    mosified

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -3,6 +3,7 @@ import 'package:ecomerce_app/components/my_product_tile.dart';
 import 'package:ecomerce_app/models/shop.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:badges/badges.dart' as badges;
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
@@ -11,6 +12,8 @@ class ShopPage extends StatelessWidget {
   Widget build(BuildContext context) {
     //access the product in the shop
     final products = context.watch<Shop>().shop;
+    final cart = context.watch<Shop>().cart;
+    final String cartItemsCount = cart.length.toString();
 
     return Scaffold(
         appBar: AppBar(
@@ -18,15 +21,30 @@ class ShopPage extends StatelessWidget {
           backgroundColor: Colors.transparent,
           elevation: 0,
           foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-          actions: [
-            IconButton(
-                padding: const EdgeInsets.only(right: 10),
-                onPressed: () => Navigator.pushNamed(context, '/cart_page'),
-                icon: Icon(
-                  Icons.shopping_cart,
-                  color: Theme.of(context).colorScheme.inversePrimary,
-                  size: 25,
-                )),
+          actions: <Widget>[
+            badges.Badge(
+              badgeContent: Text(
+                cartItemsCount,
+                style: TextStyle(color: Colors.white),
+              ),
+              showBadge: true,
+              ignorePointer: false,
+              position: badges.BadgePosition.topEnd(top: 0, end: 6),
+              badgeAnimation: const badges.BadgeAnimation.size(
+                  animationDuration: Duration(milliseconds: 500)),
+              badgeStyle: const badges.BadgeStyle(
+                badgeColor: Colors.grey,
+              ),
+              
+              child: IconButton(
+                  padding: const EdgeInsets.only(right: 10),
+                  onPressed: () => Navigator.pushNamed(context, '/cart_page'),
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    size: 25,
+                  )),
+            ),
           ],
         ),
         drawer: const MyDrawer(),
